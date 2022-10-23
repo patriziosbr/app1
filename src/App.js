adcb23160cf87adbb2cb1f3646c7bb8366a571df
patriziosbr <sbrozzi.patrizio@gmail.com>
2022-10-23 09:11:30 +0200

    hooks

--- src/App.js
+++ src/App.js
@@ -1,38 +1,16 @@
-// import * as style from './style' //-> metodo fuori dal componente
 import React from "react";
-// import Component1 from "./functional/component1";
+import Component1 from "./functional/component1"; 
 import Container1 from "./containers/container1";
 
 function App() {
-  // statornando di moda scrivere stile in linea per questioni di comodita nelle condizioni metodo dentro al componente 
-  // const style = {
-  //   border: 'solid',
-  //   backgroundColor: 'red',
-  //   height: '30px',
-  //   width: '100px'
-  // }
-  // let pippo = 'ciao'
-  // const [count, setCount] = useState(0);
-  // const var1 = {
-  //   key1: 'some data'
-  // }
 
-  return (
-    <div className="App">
-      <header className="App-header">
-        <Container1 nikcname="pippone"/>
-        {/* <Component1 name="pippo" age={25}/> */}
-        {/* react {pippo}
-        <div style={style.style_extern}></div>
-        <h2>Counter: {count}</h2>
-        <button onClick={() => setCount(count - 1) }>decrement--</button>
-        <button onClick={() => setCount(count + 1) }>increment++</button>
-        <br/>
-        {var1.key1} */}
+    return (
+      <Container1>
+        <Component1/>
+      </Container1>
 
-      </header>
-    </div>
-  );
+
+    );
 }
 
 export default App;
--- src/containers/container1.js
+++ src/containers/container1.js
@@ -1,21 +1,35 @@
 import React, { Component } from "react";
-import Component1 from "../functional/component1";
+// import Component1 from "../functional/component1";
 
 class Container1 extends Component {
-    constructor(props){
-        super(props)
-        this.state = {
-            keyOne : 'one',
-            prop2: 5
-        }
+    arr1 = [
+        {id:1,text:'text 1', number:1},
+        {id:2,text:'text 2', number:2},
+        {id:3,text:'text 3', number:3},
+        {id:4,text:'text 4', number:4}
+    ]
+
+    renderListItem = () => {
+        <div>
+
+        </div>
     }
-    // correct short hand
-    changeState = () => {
-        this.setState({prop2: this.state.prop2 + 1})
-    }
-    changeState2 = () => {
-        this.setState({keyOne: this.state.keyOne + 'l'})
-    }
+
+    ///////////////////////// setState components props
+    // constructor(props){
+    //     super(props)
+    //     this.state = {
+    //         keyOne : 'one',
+    //         prop2: 5
+    //     }
+    // }
+    // // correct short hand
+    // changeState = () => {
+    //     this.setState({prop2: this.state.prop2 + 1})
+    // }
+    // changeState2 = () => {
+    //     this.setState({keyOne: this.state.keyOne + 'l'})
+    // }
     //wrong
     // changeState = ()=> {
     //     this.state.keyOne = 'pippo';
@@ -30,16 +44,17 @@ class Container1 extends Component {
     render() {
         return (
             <div>
+                {this.arr1.map((item, index) => console.log(index))}
                 {/* props: {this.props.nikcname}
                 <br/>
-                state: {this.state.prop2} */}
+                state: {this.state.prop2}
                 <br/>
                 <button onClick={()=> this.changeState()}>change state</button>
                 <br/>
                 <button onClick={()=> this.changeState2()}>change state2</button>
-                state: {this.state.keyOne}
+                state: {this.state.keyOne} */}
 
-                <Component1 prop1={this.state.prop2}/>
+                {/* <Component1 prop1={this.state.prop2}/> */}
             </div>
         )}
 }
